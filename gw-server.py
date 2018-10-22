81c7b0239d75d910eba4c0bf4b0f2857953765eb
Matevz Vucnik <vucnikm@gmail.com>
2018-10-22 17:21:18 +0200

    add systemd service

--- gw-server.py
+++ gw-server.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 class GwRPC(object):
 	def set_data(self, device_id, source_id, measurements):
 		try:		
-			client.publish("saam_data/"+device_id+"/"+source_id, json.dumps(measurements))
+			client.publish("saam/"+device_id+"/"+source_id, json.dumps(measurements))
 			return True
 		except:
 			return False
